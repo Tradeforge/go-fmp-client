bca69242800b57d9d307115baad36ec76c65126d
Marek Cermak <marek.cermak@strv.com>
2025-01-04 10:16:05 +0100

    feat: add insider trading and sec RSS feed
    Signed-off-by: Marek Cermak <marek.cermak@strv.com>

--- market/event.go
+++ market/event.go
@@ -11,10 +11,54 @@ import (
 const (
 	GetEarningsCalendarPath   = "/api/v3/earning_calendar"
 	GetHistoricalEarningsPath = "/api/v3/historical/earning_calendar/:symbol"
+	ListSECFilingsRSSFeedPath = "/api/v4/rss_feed"
+	ListInsiderTradesPath     = "/api/v4/insider-trading"
+
+	defaultSECFeedLimit = 100
 )
 
 type EventClient struct {
 	*rest.Client
+}
+
+func (ec *EventClient) ListInsiderTrades(ctx context.Context, params model.ListInsiderTradesParams, opts ...model.RequestOption) (model.ListInsiderTradesResponse, error) {
+	var res model.ListInsiderTradesResponse
+	_, err := ec.Call(ctx, http.MethodGet, ListInsiderTradesPath, params, &res, opts...)
+	return res, err
+}
+
+func (ec *EventClient) ListSECFilingsRSSFeed(ctx context.Context, params model.ListSECFilingsRSSFeedParams, opts ...model.RequestOption) (model.ListSECFilingsRSSFeedResponse, error) {
+	r, err := ec.doListSECFilingsRSSFeed(ctx, params, opts...)
+	if err != nil {
+		return nil, err
+	}
+	filteredResults := make(model.ListSECFilingsRSSFeedResponse, 0, len(r))
+L:
+	for {
+		for _, filing := range r {
+			if filing.Type.Form == params.Type {
+				filteredResults = append(filteredResults, filing)
+			}
+			if len(filteredResults) == len(r) {
+				break L
+			}
+		}
+		r, err = ec.doListSECFilingsRSSFeed(ctx, params, opts...)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return filteredResults, nil
+}
+
+func (ec *EventClient) doListSECFilingsRSSFeed(ctx context.Context, params model.ListSECFilingsRSSFeedParams, opts ...model.RequestOption) (model.ListSECFilingsRSSFeedResponse, error) {
+	var limit uint = defaultSECFeedLimit
+	if params.Limit == nil {
+		params.Limit = &limit
+	}
+	var res model.ListSECFilingsRSSFeedResponse
+	_, err := ec.Call(ctx, http.MethodGet, ListSECFilingsRSSFeedPath, params, &res, opts...)
+	return res, err
 }
 
 func (ec *EventClient) GetEarningsCalendar(ctx context.Context, params *model.GetEarningsCalendarParams, opts ...model.RequestOption) ([]model.GetEarningsCalendarResponse, error) {
