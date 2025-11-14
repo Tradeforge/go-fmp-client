6389a969d828ca12fc8bf0f50f6a72042c4482eb
Marek Cermak <marek.cermak@strv.com>
2025-11-14 10:52:22 +0100

    break: add realtime news per symbol and change existing to latest
    Signed-off-by: Marek Cermak <marek.cermak@strv.com>

--- client/rest/client.go
+++ client/rest/client.go
@@ -1,144 +1,144 @@
 package rest
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "log/slog"
-    "slices"
-    "time"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"slices"
+	"time"
 
-    "github.com/go-resty/resty/v2"
+	"github.com/go-resty/resty/v2"
 
-    "go.tradeforge.dev/fmp/encoder"
-    "go.tradeforge.dev/fmp/model"
+	"go.tradeforge.dev/fmp/encoder"
+	"go.tradeforge.dev/fmp/model"
 )
 
 const (
-    apiURL        = "https://financialmodelingprep.com"
-    clientVersion = "v0.0.0"
+	apiURL        = "https://financialmodelingprep.com"
+	clientVersion = "v0.0.0"
 
-    DefaultRetryCount    = 3
-    DefaultClientTimeout = 300 * time.Second
+	DefaultRetryCount    = 3
+	DefaultClientTimeout = 300 * time.Second
 )
 
 func New(
-    apiKey string,
-    logger *slog.Logger,
+	apiKey string,
+	logger *slog.Logger,
 ) *Client {
-    c := resty.New()
+	c := resty.New()
 
-    c.SetBaseURL(apiURL)
-    c.SetRetryCount(DefaultRetryCount)
-    c.SetTimeout(DefaultClientTimeout)
-    c.SetHeader("User-Agent", fmt.Sprintf("Tradeforge client/%v", clientVersion))
-    c.SetHeader("Accept", "application/json")
-    c.SetQueryParam("apikey", apiKey)
+	c.SetBaseURL(apiURL)
+	c.SetRetryCount(DefaultRetryCount)
+	c.SetTimeout(DefaultClientTimeout)
+	c.SetHeader("User-Agent", fmt.Sprintf("Tradeforge client/%v", clientVersion))
+	c.SetHeader("Accept", "application/json")
+	c.SetQueryParam("apikey", apiKey)
 
-    return &Client{
-        HTTP:    c,
-        encoder: encoder.New(),
-        logger:  logger,
-    }
+	return &Client{
+		HTTP:    c,
+		encoder: encoder.New(),
+		logger:  logger,
+	}
 }
 
 // Client defines an HTTP client for the Polygon REST API.
 type Client struct {
-    HTTP    *resty.Client
-    encoder *encoder.Encoder
-    logger  *slog.Logger
+	HTTP    *resty.Client
+	encoder *encoder.Encoder
+	logger  *slog.Logger
 }
 
 // Call makes an API call based on the request params and options. The response is automatically unmarshaled.
 func (c *Client) Call(ctx context.Context, method, path string, params, response any, opts ...model.RequestOption) (*resty.Response, error) {
-    uri, err := c.encoder.EncodeParams(path, params)
-    if err != nil {
-        return nil, fmt.Errorf("encoding params: %w", err)
-    }
-    return c.CallURL(ctx, method, uri, response, opts...)
+	uri, err := c.encoder.EncodeParams(path, params)
+	if err != nil {
+		return nil, fmt.Errorf("encoding params: %w", err)
+	}
+	return c.CallURL(ctx, method, uri, response, opts...)
 }
 
 // CallURL makes an API call based on a request URI and options. The response is automatically unmarshaled.
 func (c *Client) CallURL(ctx context.Context, method, uri string, response any, opts ...model.RequestOption) (*resty.Response, error) {
-    options := mergeOptions(opts...)
+	options := mergeOptions(opts...)
 
-    c.HTTP.SetTimeout(DefaultClientTimeout)
-    req := c.HTTP.R().SetContext(ctx)
-    if options.Body != nil {
-        b, err := json.Marshal(options.Body)
-        if err != nil {
-            return nil, fmt.Errorf("failed to marshal body: %w", err)
-        }
-        req.SetBody(b)
-    }
-    req.SetQueryParamsFromValues(options.QueryParams)
-    req.SetHeaderMultiValues(options.Headers)
-    req.SetResult(response).SetError(&model.ResponseError{})
-    req.SetHeader("Content-Type", options.ContentType)
+	c.HTTP.SetTimeout(DefaultClientTimeout)
+	req := c.HTTP.R().SetContext(ctx)
+	if options.Body != nil {
+		b, err := json.Marshal(options.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal body: %w", err)
+		}
+		req.SetBody(b)
+	}
+	req.SetQueryParamsFromValues(options.QueryParams)
+	req.SetHeaderMultiValues(options.Headers)
+	req.SetResult(response).SetError(&model.ResponseError{})
+	req.SetHeader("Content-Type", options.ContentType)
 
-    res, err := req.Execute(method, uri)
-    if err != nil {
-        return nil, fmt.Errorf("failed to execute request: %w", err)
-    }
-    if res.IsError() {
-        if slices.Contains(options.IgnoredErrorStatusCodes, res.StatusCode()) {
-            return res, nil
-        }
-        responseError := parseResponseError(res)
-        if responseError != nil {
-            c.logger.Error(
-                "response error",
-                slog.String("url", uri),
-                slog.Int("status", responseError.StatusCode),
-                slog.String("error message", responseError.ErrorMessage),
-            )
-        } else {
-            c.logger.Error(
-                "response error",
-                slog.String("url", uri),
-                slog.Int("status", res.StatusCode()),
-                slog.String("error message", res.Status()),
-                slog.String("response", string(res.Body())),
-            )
-        }
-        return res, fmt.Errorf("service responded with an unexpected error code: %w", responseError)
-    }
+	res, err := req.Execute(method, uri)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute request: %w", err)
+	}
+	if res.IsError() {
+		if slices.Contains(options.IgnoredErrorStatusCodes, res.StatusCode()) {
+			return res, nil
+		}
+		responseError := parseResponseError(res)
+		if responseError != nil {
+			c.logger.Error(
+				"response error",
+				slog.String("url", uri),
+				slog.Int("status", responseError.StatusCode),
+				slog.String("error message", responseError.ErrorMessage),
+			)
+		} else {
+			c.logger.Error(
+				"response error",
+				slog.String("url", uri),
+				slog.Int("status", res.StatusCode()),
+				slog.String("error message", res.Status()),
+				slog.String("response", string(res.Body())),
+			)
+		}
+		return res, fmt.Errorf("service responded with an unexpected error code: %w", responseError)
+	}
 
-    if options.Trace {
-        sanitizedHeaders := req.Header
-        for k := range sanitizedHeaders {
-            if k == "Authorization" {
-                sanitizedHeaders[k] = []string{"REDACTED"}
-            }
-        }
-        c.logger.Debug(
-            "request",
-            slog.String("url", uri),
-            slog.Any("request headers", sanitizedHeaders),
-            slog.Any("response headers", res.Header()),
-        )
-    }
-    return res, nil
+	if options.Trace {
+		sanitizedHeaders := req.Header
+		for k := range sanitizedHeaders {
+			if k == "Authorization" {
+				sanitizedHeaders[k] = []string{"REDACTED"}
+			}
+		}
+		c.logger.Debug(
+			"request",
+			slog.String("url", uri),
+			slog.Any("request headers", sanitizedHeaders),
+			slog.Any("response headers", res.Header()),
+		)
+	}
+	return res, nil
 }
 
 func mergeOptions(opts ...model.RequestOption) *model.RequestOptions {
-    options := &model.RequestOptions{
-        ContentType: "application/json",
-    }
-    for _, o := range opts {
-        o(options)
-    }
+	options := &model.RequestOptions{
+		ContentType: "application/json",
+	}
+	for _, o := range opts {
+		o(options)
+	}
 
-    return options
+	return options
 }
 
 func parseResponseError(res *resty.Response) *model.ResponseError {
-    if res == nil {
-        return nil
-    }
-    responseError := res.Error().(*model.ResponseError)
-    responseError.StatusCode = res.StatusCode()
-    responseError.ErrorMessage = res.String()
+	if res == nil {
+		return nil
+	}
+	responseError := res.Error().(*model.ResponseError)
+	responseError.StatusCode = res.StatusCode()
+	responseError.ErrorMessage = res.String()
 
-    return responseError
+	return responseError
 }
--- market/news.go
+++ market/news.go
@@ -1,58 +1,79 @@
 package market
 
 import (
-	"context"
-	"net/http"
+    "context"
+    "net/http"
 
-	"go.tradeforge.dev/fmp/client/rest"
-	"go.tradeforge.dev/fmp/model"
+    "go.tradeforge.dev/fmp/client/rest"
+    "go.tradeforge.dev/fmp/model"
 )
 
 const (
-	GetFMPArticlesPath   = "/stable/fmp-articles"
-	GetGeneralNewsPath   = "/stable/news/general-latest"
-	GetPressReleasesPath = "/stable/news/press-releases-latest"
-	GetStockNewsPath     = "/stable/news/stock-latest"
-	GetCryptoNewsPath    = "/stable/news/crypto-latest"
-	GetForexNewsPath     = "/stable/news/forex-latest"
+    GetFMPArticlesPath      = "/stable/fmp-articles"
+    GetGeneralNewsPath      = "/stable/news/general-latest"
+    GetPressReleasesPath    = "/stable/news/press-releases-latest"
+    GetLatestStockNewsPath  = "/stable/news/stock-latest"
+    GetStockNewsPath        = "/stable/news/stock"
+    GetLatestCryptoNewsPath = "/stable/news/crypto-latest"
+    GetCryptoNewsPath       = "/stable/news/crypto"
+    GetLatestForexNewsPath  = "/stable/news/forex-latest"
+    GetForexNewsPath        = "/stable/news/forex"
 )
 
 type NewsClient struct {
-	*rest.Client
+    *rest.Client
 }
 
 func (nc *NewsClient) GetFMPArticles(ctx context.Context, params model.GetFMPArticlesParams, opts ...model.RequestOption) (model.GetFMPArticlesResponse, error) {
-	var res model.GetFMPArticlesResponse
-	_, err := nc.Call(ctx, http.MethodGet, GetFMPArticlesPath, params, &res, opts...)
-	return res, err
+    var res model.GetFMPArticlesResponse
+    _, err := nc.Call(ctx, http.MethodGet, GetFMPArticlesPath, params, &res, opts...)
+    return res, err
 }
 
 func (nc *NewsClient) GetGeneralNews(ctx context.Context, params model.GetNewsParams, opts ...model.RequestOption) (model.GetNewsResponse, error) {
-	var res model.GetNewsResponse
-	_, err := nc.Call(ctx, http.MethodGet, GetGeneralNewsPath, params, &res, opts...)
-	return res, err
+    var res model.GetNewsResponse
+    _, err := nc.Call(ctx, http.MethodGet, GetGeneralNewsPath, params, &res, opts...)
+    return res, err
 }
 
 func (nc *NewsClient) GetPressReleases(ctx context.Context, params model.GetNewsParams, opts ...model.RequestOption) (model.GetNewsResponse, error) {
-	var res model.GetNewsResponse
-	_, err := nc.Call(ctx, http.MethodGet, GetPressReleasesPath, params, &res, opts...)
-	return res, err
+    var res model.GetNewsResponse
+    _, err := nc.Call(ctx, http.MethodGet, GetPressReleasesPath, params, &res, opts...)
+    return res, err
+}
+
+func (nc *NewsClient) GetLatestStockNews(ctx context.Context, params model.GetLatestNewsParams, opts ...model.RequestOption) (model.GetNewsResponse, error) {
+    var res model.GetNewsResponse
+    _, err := nc.Call(ctx, http.MethodGet, GetLatestStockNewsPath, params, &res, opts...)
+    return res, err
 }
 
 func (nc *NewsClient) GetStockNews(ctx context.Context, params model.GetNewsParams, opts ...model.RequestOption) (model.GetNewsResponse, error) {
-	var res model.GetNewsResponse
-	_, err := nc.Call(ctx, http.MethodGet, GetStockNewsPath, params, &res, opts...)
-	return res, err
+    var res model.GetNewsResponse
+    _, err := nc.Call(ctx, http.MethodGet, GetStockNewsPath, params, &res, opts...)
+    return res, err
+}
+
+func (nc *NewsClient) GetLatestCryptoNews(ctx context.Context, params model.GetLatestNewsParams, opts ...model.RequestOption) (model.GetNewsResponse, error) {
+    var res model.GetNewsResponse
+    _, err := nc.Call(ctx, http.MethodGet, GetLatestCryptoNewsPath, params, &res, opts...)
+    return res, err
 }
 
 func (nc *NewsClient) GetCryptoNews(ctx context.Context, params model.GetNewsParams, opts ...model.RequestOption) (model.GetNewsResponse, error) {
-	var res model.GetNewsResponse
-	_, err := nc.Call(ctx, http.MethodGet, GetCryptoNewsPath, params, &res, opts...)
-	return res, err
+    var res model.GetNewsResponse
+    _, err := nc.Call(ctx, http.MethodGet, GetCryptoNewsPath, params, &res, opts...)
+    return res, err
+}
+
+func (nc *NewsClient) GetLatestForexNews(ctx context.Context, params model.GetLatestNewsParams, opts ...model.RequestOption) (model.GetNewsResponse, error) {
+    var res model.GetNewsResponse
+    _, err := nc.Call(ctx, http.MethodGet, GetLatestForexNewsPath, params, &res, opts...)
+    return res, err
 }
 
 func (nc *NewsClient) GetForexNews(ctx context.Context, params model.GetNewsParams, opts ...model.RequestOption) (model.GetNewsResponse, error) {
-	var res model.GetNewsResponse
-	_, err := nc.Call(ctx, http.MethodGet, GetForexNewsPath, params, &res, opts...)
-	return res, err
+    var res model.GetNewsResponse
+    _, err := nc.Call(ctx, http.MethodGet, GetForexNewsPath, params, &res, opts...)
+    return res, err
 }
